3ce23af281d1432b9f9b22d4fb4500c51e94e232
Anil Pujari <pujarianil4@gmail.com>
2025-03-27 08:24:46 +0530

    remove unwanted network config

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,8 +1,8 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {  sepolia, mainnet } from 'wagmi/chains';
+import {  sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Mini-defi',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [sepolia, mainnet],
+  chains: [sepolia],
 });
